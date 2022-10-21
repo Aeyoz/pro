748240a46036a3d843ce522a7be09ccd8d3dfb8b
ElPayo <elpayov2@gmail.com>
2022-10-21 16:09:41 +0100

    Archivos

---
+++ ut2/2_loops/ejercicio1.py
@@ -0,0 +1,7 @@
+num = 47
+sum = 0
+
+while sum <= num:
+    sum += 3
+else:
+    print(sum)
---
+++ ut2/2_loops/ejercicio2.py
@@ -0,0 +1,8 @@
+word = "Supercalifragilisticoespialidoso"
+vowels = "aeiou"
+vowels_count = 0
+
+for i in word:
+    if i in vowels:
+        vowels_count += 1
+print(vowels_count)
