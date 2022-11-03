4db0f7510afcc80107c7601d649ecef6360d2be6
ElPayo <elpayov2@gmail.com>
2022-11-03 18:29:41 +0000

    Errores

--- ut2/2_loops/ejercicios.py/ejercicio13.py
+++ ut2/2_loops/ejercicios.py/ejercicio13.py
@@ -1,12 +1,12 @@
 tries = 0
 num = 13
 
-while guess:= int(input("Introduce a number between 0 and 100: ")) != num:
+while guess:= (int(input("Introduce a number between 0 and 100: "))) != num:
     tries += 1
     if guess > num: 
-        print("The number introduced was too big")
+        print("Menor")
     elif guess < num:
-        print("The number introduced was too low")
-    else:
-        print(f"Wow, it only took you {tries} tries to guess")
-        break
+        print("Mayor")
+
+tries += 1
+print(f"Wow, it only took you {tries} tries to guess")
