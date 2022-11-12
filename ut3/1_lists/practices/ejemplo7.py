78cd1f8d016c81e1a0b50ab0290e9dd3fb9263d3
ElPayo <elpayov2@gmail.com>
2022-11-12 12:27:48 +0000

    Matrix completada

--- ut3/1_lists/practices/ejemplo7.py
+++ ut3/1_lists/practices/ejemplo7.py
@@ -1,40 +1,10 @@
 matrix_A = [[6, 4],[8, 9]]
 matrix_B = [[3, 2],[1, 7]]
-matrix_C = []
+matrix_C = [[],[]]
 
-# for rows in matrix_A[0]:
-#     for j in rows[0][0]:
-#         result = matrix_A[rows][j]
-#         print(matrix_A[rows][j])
+matrix_C[0].append(matrix_A[0][0] * matrix_B[0][0] + matrix_A[0][1] * matrix_B[1][0])
+matrix_C[0].append(matrix_A[0][0] * matrix_B[0][1] + matrix_A[0][1] * matrix_B[1][1])
+matrix_C[1].append(matrix_A[1][0] * matrix_B[0][0] + matrix_A[1][1] * matrix_B[1][0])
+matrix_C[1].append(matrix_A[1][0] * matrix_B[0][1] + matrix_A[1][1] * matrix_B[1][1])
 
-pos1 = 0
-pos2 = 0
-for _A in matrix_A:
-    for i in range(len(_A)):
-        element1 = _A[i]
-        for _B in matrix_B:
-            for j in range(len(_B)):
-                element2 = _B[j]
-                pos1 = element1 * element2
-                print(pos1)
-# for _A, _B in zip(matrix_A, matrix_B):
-#     pos1 = 0
-#     pos2 = 0
-#     for value_A in _A:
-#         for value_B in _B:
-#             pos1 += value_A * value_B
-#             break
-#     for value_B in _B:
-#         for value_A in _A:
-#             ""
-#         mult = pos1 + pos2
-#         matrix_C.append(mult)
-# print(matrix_C)
-
-
-
-# for _A, _B in zip(matrix_A, matrix_B):
-#     for value_A, value_B in zip(_A, _B):
-#         mult = value_A * value_B
-#         matrix_C.append(mult)
-# print(matrix_C)
+print(matrix_C)
