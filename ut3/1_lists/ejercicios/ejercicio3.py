af1eae497e4f812a535f4791b6d51b9284a0df99
ElPayo <elpayov2@gmail.com>
2022-11-21 17:39:39 +0000

    Actualizacion del repositorio

--- ut3/1_lists/ejercicios/ejercicio3.py
+++ ut3/1_lists/ejercicios/ejercicio3.py
@@ -1,9 +1,11 @@
 nested_values = [0, 10, [20, 30], 40, 50, [60, 70, 80], [90, 100, 110, 120]]
-flatted_values = []
+flattened_values = []
 for element in nested_values:
-    if type(element) == list:
-        for sub_element in element:
-            flatted_values.append(sub_element)
+    #if type(element) == list:
+    if isinstance(element, list):
+        flattened_values.extend(element)
+        #for sub_element in element:
+            #flattened_values.append(sub_element)
     else:
-        flatted_values.append(element)
-print(flatted_values)   
+        flattened_values.append(element)
+print(flattened_values)   
--- ut3/1_lists/ejercicios/ejercicio5.py
+++ ut3/1_lists/ejercicios/ejercicio5.py
@@ -1,6 +1,6 @@
-nums = [1, 1, 1, 1, 1, 1, 1]
+values = [1, 1, 1, 1, 1, 1, 1]
 
-for i, j in zip(nums, nums[1:]):
+for i, j in zip(values, values[1:]):
     if i != j:
         print("No son iguales")
         break
