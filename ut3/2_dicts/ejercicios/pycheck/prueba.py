3d8df64633256d3227e9550e93d06e922e8e1e13
ElPayo <elpayov2@gmail.com>
2023-01-26 20:31:37 +0000

    del sor

--- ut3/2_dicts/ejercicios/pycheck/prueba.py
+++ ut3/2_dicts/ejercicios/pycheck/prueba.py
@@ -1,14 +1,5 @@
 unsorted_items = {'C1': 'Red', 'C2': 'Black', 'C3': 'Red', 'C4': 'Green'}
-sorted_items = []
-unsorted_items_2 = {}
-same_key_items = []
-for key, value in unsorted_items.items():
-    if value not in unsorted_items_2:
-        unsorted_items_2[value] = key
-    else:
-        same_key_items.append(tuple((key,value)))
-sorted_values = sorted(list(unsorted_items_2.items()))
-values = {tupple[1]:tupple[0] for tupple in sorted_values}
-values[same_key_items[0]] = same_key_items[1]
-sorted_items = list(values.items())
-print(sorted_items)
+values_then_keys = [(f"{v}:{k}") for k,v in unsorted_items.items()]
+values_then_keys.sort()
+sorted_values = [(item[0], item[1]) for item in values_then_keys.split(":")]
+print(sorted_values)
