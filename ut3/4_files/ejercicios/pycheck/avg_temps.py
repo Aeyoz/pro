92fa8c39a2a921321b8535cd5bc2a2c4b07e0973
ElPayo <elpayov2@gmail.com>
2023-01-27 17:15:19 +0000

    ficheros

--- ut3/4_files/ejercicios/pycheck/avg_temps.py
+++ ut3/4_files/ejercicios/pycheck/avg_temps.py
@@ -7,14 +7,13 @@ from pathlib import Path
 
 def run(input_path: Path) -> bool:
     output_path = 'data/avg_temps/avg_temps.dat'
-    with open("data/avg_temps/temperatures.dat") as data:
+    with open(input_path) as data:
         avg_temps = open(output_path, "w")
         for month in data:
-            temps = [int(temp) for temp in month.split(",")]
+            temps = [int(temp) for temp in month.strip().split(",")]
             avg_temp = (sum(temps) / len(temps))
-            avg_temps.write(f"{avg_temp}\n")
-    
-    avg_temps.close()    
+            avg_temps.write(f"{avg_temp:.2f}\n")
+        avg_temps.close()    
 
     return filecmp.cmp(output_path, 'data/avg_temps/.expected', shallow=False)
 
