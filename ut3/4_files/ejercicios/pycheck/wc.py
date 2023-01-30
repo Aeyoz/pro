103771878dc93d93cf8f2c62799068273ee01a6c
ElPayo <elpayov2@gmail.com>
2023-01-30 18:02:00 +0000

    pokemon

--- ut3/4_files/ejercicios/pycheck/wc.py
+++ ut3/4_files/ejercicios/pycheck/wc.py
@@ -6,10 +6,10 @@ from pathlib import Path
 def run(input_path: Path) -> tuple:
     num_lines = num_words = num_bytes = 0
     with open(input_path) as lines:
-        num_lines = len(lines.readlines())
         for line in lines:
             num_bytes += len(line.encode('utf-8'))
             num_words += len(line.split())
+            num_lines += 1
     return num_lines, num_words, num_bytes
 
 
