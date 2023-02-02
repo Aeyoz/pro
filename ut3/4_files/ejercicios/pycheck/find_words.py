c7ae380002fde2d536d90ab4f8a84d33898f0dcd
ElPayo <elpayov2@gmail.com>
2023-02-02 12:17:59 +0000

    ejercicios

---
+++ ut3/4_files/ejercicios/pycheck/find_words.py
@@ -0,0 +1,14 @@
+# ***********************************
+# �D�NDE EST�N LAS PALABRAS? MATARILE
+# ***********************************
+from pathlib import Path
+
+
+def run(data_path: Path, target_word: str) -> list:
+    matches = 'output'
+    
+    return matches
+
+
+if __name__ == '__main__':
+    run('data/find_words/bzrp.txt', 'persona')
---
+++ ut3/4_files/ejercicios/pycheck/replace_chars.py
@@ -0,0 +1,18 @@
+# ***********************
+# REEMPLAZO DE CARACTERES
+# ***********************
+import filecmp
+from pathlib import Path
+
+
+def run(input_path: Path, replacements: str) -> bool:
+    output_path = 'data/replace_chars/r_noticia.txt'
+    with open(input_path) as f:
+        document = f.readlines()
+        text = "".join(document)
+        print(text)
+    return filecmp.cmp(output_path, 'data/replace_chars/.expected', shallow=False)
+
+
+if __name__ == '__main__':
+    run('data/replace_chars/noticia.txt', 'áa|ée|íi|óo|úu')
