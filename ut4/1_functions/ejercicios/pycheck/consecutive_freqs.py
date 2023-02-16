b5306d7a99400dd7825e782acc3fb9a608bd4148
ElPayo <elpayov2@gmail.com>
2023-02-16 18:18:13 +0000

    consecutive freqs mejorado

--- ut4/1_functions/ejercicios/pycheck/consecutive_freqs.py
+++ ut4/1_functions/ejercicios/pycheck/consecutive_freqs.py
@@ -2,33 +2,21 @@
 # FRECUENCIA DE ELEMENTOS CONSECUTIVOS
 # ************************************
 
-
 def cfreq(values, as_string=False):
-    output = []
+    freqs = []
     counter = 0
 
-    if not values:
-        if not as_string:
-            return output
-        elif as_string:
-            return ""
-    prev_v = values[0]
-    
-    for value in values[1:]:
+    if len(values) > 0:
+        prev_v = values[0]
+        for value in values[1:]:
+            counter += 1
+            if value != prev_v:
+                freqs.append((prev_v, counter))
+                prev_v = value
+                counter = 0
         counter += 1
-        if value != prev_v:
-            output.append((prev_v, counter))
-            prev_v = value
-            counter = 0
-    counter += 1
-    output.append((prev_v, counter))
-    
-    if not as_string:
-        return output
-    else:
-        freqs = []
-        for t in output:
-            item1, item2 = t
-            freqs.append(f"{item1}:{item2}")
-        freq = ",".join(freqs)
-        return freq
+        freqs.append((prev_v, counter))
+
+    if as_string:
+        freqs = ",".join([f"{i}:{f}" for i, f in freqs])
+    return freqs
