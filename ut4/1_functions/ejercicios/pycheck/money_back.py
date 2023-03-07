647e2efd37eec836e910204aabb014ca5d738759
ElPayo <elpayov2@gmail.com>
2023-03-07 17:39:19 +0000

    cambios deprecated

--- ut4/1_functions/ejercicios/pycheck/money_back.py
+++ ut4/1_functions/ejercicios/pycheck/money_back.py
@@ -6,11 +6,12 @@
 def run(to_give_back: float, available_currencies: list) -> dict:
     money_back = {}
     for currency in sorted(available_currencies, reverse=True):
-        if not to_give_back:
+        if to_give_back:
+            return_currency = to_give_back // currency
+            to_give_back %= currency
+            money_back[currency] = money_back.get(currency, 0) + return_currency
+        else:
             return money_back
-        return_currency = to_give_back // currency
-        to_give_back %= currency
-        money_back[currency] = money_back.get(currency, 0) + return_currency
     if to_give_back > 0:
         return None
     return money_back
--- ut4/1_functions/ejercicios/pycheck/money_back_max.py
+++ ut4/1_functions/ejercicios/pycheck/money_back_max.py
@@ -6,13 +6,12 @@
 def run(to_give_back: float, available_currencies: dict) -> dict:
     money_back = {} 
     for currency in sorted(available_currencies, reverse=True):
-        return_currency = to_give_back // currency
-        amount = available_currencies[currency]
-        if return_currency > amount:
-            return_currency -= return_currency - amount
-        to_give_back -= currency * return_currency
-        money_back[currency] = return_currency
-    money_back = {currency: amount for currency, amount in money_back.items() if amount > 0}
+        if to_give_back > 0:
+            return_currency = to_give_back // currency
+            amount = available_currencies[currency]
+            _min = min(amount, return_currency)
+            to_give_back -= currency * _min
+            money_back[currency] = _min
     if to_give_back > 0:
         money_back = None
     return money_back
