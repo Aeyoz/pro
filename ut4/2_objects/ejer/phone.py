cec11a1ac5e696fa257999e4076206e0851ddd21
ElPayo <elpayov2@gmail.com>
2023-03-23 18:30:20 +0000

    phone

--- ut4/2_objects/ejer/phone.py
+++ ut4/2_objects/ejer/phone.py
@@ -1,7 +1,9 @@
 CONSUMPTION_CHART = {"POWER_ON" : 5, "POWER_OFF" : 2.5, "APP_POWER" : 1}
 
 class MobilePhone:
-    def __init__(self, manufacturer: str, screen_size: float, num_cores: int, apps: list[str] = [], status: bool = False, battery_level: float = 100):
+    def __init__(self, manufacturer: str, screen_size: float, num_cores: int\
+        , apps: list[str] = ["whatsapp", "instagram", "chrome"], status: bool =\
+        False, battery_level: float = 100):
         self.manufacturer = manufacturer
         self.screen_size = screen_size
         self.num_cores = num_cores
@@ -9,27 +11,21 @@ class MobilePhone:
         self.status = status
         self.battery = battery_level
 
-    def recharge(self, recharge_battery_level):
-        for _ in range((recharge_battery_level + 1) * 2):
-            if self.battery != 100.0:
-                self.battery += 0.5
+    def recharge(self, recharge_battery_level) -> None:
+        for i in range(recharge_battery_level + 1):
+            if i == recharge_battery_level:
+                break
+            self.battery += 0.5 if 100 - self.battery < 1 else 1
 
-    def take_of_battery_unit(self, battery_level=CONSUMPTION_CHART["APP_POWER"]):
-        self.battery -= battery_level
+    def battery_drainage(self, operation_type, dreinage_chart = CONSUMPTION_CHART) -> None:
+        self.battery -= dreinage_chart[operation_type.upper()]
 
-    def power_on(self, battery_level = CONSUMPTION_CHART["POWER_ON"]):
-        if self.status:
-            return f"Phone already on"
-        self.status = True
-        self.take_of_battery_unit(battery_level)
+    def switch_status(self, operation_type: str = "power_on" ) -> None:
+        self.status = not self.status
+        operation_type = "power_on" if self.status else "power_off"
+        self.battery_drainage(operation_type)
 
-    def power_off(self, battery_level = CONSUMPTION_CHART["POWER_OFF"]):
-        if not self.status:
-            return f"Phone already off"
-        self.status = False
-        self.take_of_battery_unit(battery_level)
-
-    def install(self, *apps):
+    def install_apps(self, *apps) -> str:
         error_msgs = []
         if self.status == 0:
             return f"The device has to be on"
@@ -38,29 +34,19 @@ class MobilePhone:
                 self.apps.append(app)
             else:
                 error_msgs.append(f"Unable to install {app}, already installed")
-            self.take_of_battery_unit()
-        return error_msgs
+            self.battery_drainage("app_power")
+        return "\n".join(error_msgs)
 
-    def uninstall(self, *apps):
+    def uninstall_apps(self, *apps) -> str:
         error_msgs = []
         if self.status == 0:
             return f"The device has to be on"
-        for app in apps:
+        for index, app in enumerate(apps):
             if app in self.apps:
-                index = self.apps.index(app)
                 self.apps.pop(index)
             else:
                 error_msgs.append(f"Unable to install {app}, already installed")
-            self.take_of_battery_unit()
-        return error_msgs
+            self.battery_drainage("app_power")
+        return "\n".join(error_msgs)
 
-
-samsung = MobilePhone("Samsung", 7.5, 6, ["whatsapp", "reddit", "discord"], status=True)
-
-samsung.install("aliexpress", "amazon", "duolingo")
-samsung.install("zzzzz", "sssss", "duolingo")
-samsung.uninstall("zzzzz", "sssss", "duolingo", "asdasdw")
-samsung.power_on()
-samsung.power_off()
-
-print(samsung.battery)
+samsung = MobilePhone("Samsung", 7.5, 6)
