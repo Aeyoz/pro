98bb2547e7632c9e277728b7c5c5e17ae5ed2ec3
ElPayo <elpayov2@gmail.com>
2023-03-24 17:15:07 +0000

    mejorar nombres

--- ut4/2_objects/ejer/phone.py
+++ ut4/2_objects/ejer/phone.py
@@ -1,10 +1,17 @@
 CONSUMPTION_CHART = {"POWER_ON" : 5, "POWER_OFF" : 2.5, "APP_POWER" : 1}
 FULL = 100
 EMPTY = 0
+
 class MobilePhone:
-    def __init__(self, manufacturer: str, screen_size: float, num_cores: int\
-        , apps: list[str] = ["whatsapp", "instagram", "chrome"], status: bool =\
-        True, battery_level: float = 100):
+    def __init__(
+    self,
+    manufacturer: str,
+    screen_size: float,
+    num_cores: int,
+    apps: list[str] = ["whatsapp", "instagram", "chrome"],
+    status: bool = True,
+    battery_level: float = FULL):
+    
         self.manufacturer = manufacturer
         self.screen_size = screen_size
         self.num_cores = num_cores
@@ -13,19 +20,18 @@ class MobilePhone:
         self.battery = battery_level
 
     def recharge(self, recharge_battery_level) -> None:
-        for i in range(recharge_battery_level + 1):
-            if i == recharge_battery_level or self.battery == FULL:
-                break
-            self.battery += 0.5 if 100 - self.battery < 1 else 1
+        recharge = min(FULL, self.battery + recharge_battery_level)
+        self.battery = recharge
 
     def battery_drainage(self, operation_type, dreinage_chart = CONSUMPTION_CHART) -> tuple:
         battery_taken = dreinage_chart[operation_type.upper()]
         if self.battery - battery_taken > 0:
             self.battery -= battery_taken
-            return True, "The phone has battery"
+            output = (True, "The phone has battery")
         if self.battery - battery_taken <= 0:
             self.status, self.battery = False, EMPTY
-            return False, "The phone has run out of battery"
+            output = (False, "The phone has run out of battery")
+        return output
 
     def switch_status(self, operation_type: str = "power_on" ) -> None:
         self.status = not self.status
@@ -34,8 +40,8 @@ class MobilePhone:
 
     def install_apps(self, *apps) -> str:
         error_msgs = []
-        if self.status == 0:
-            return f"The device has to be on"
+        if not self.status:
+            return "The device has to be on"
         for app in apps:
             if app not in self.apps:
                 self.apps.append(app)
@@ -48,8 +54,8 @@ class MobilePhone:
 
     def uninstall_apps(self, *apps) -> str| bool:
         error_msgs = []
-        if self.status == 0:
-            return f"The device has to be on"
+        if not self.status:
+            return "The device has to be on"
         for index, app in enumerate(apps):
             if app in self.apps:
                 self.apps.pop(index)
@@ -60,14 +66,10 @@ class MobilePhone:
                 return msg
         return "\n".join(error_msgs)
 
-
-
-
-output = len(["dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede"])
-print(output * 2)
-
 samsung = MobilePhone("Samsung", 7.5, 6)
 print(samsung.status)
+samsung.install_apps("dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede")
+samsung.install_apps("dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede")
 samsung.install_apps("dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede")
 samsung.install_apps("dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede","dede")
 print(samsung.battery)
