38c143a86f551eaac2e159f41f2f2ea001fc7b0f
ElPayo <elpayov2@gmail.com>
2023-03-30 10:26:12 +0100

    os

--- ut4/2_objects/ejer/os2.py
+++ ut4/2_objects/ejer/os2.py
@@ -1,8 +1,8 @@
 class OS:
-    BOOTED_ERROR = "Turn on the OS"
-    INVALID_FOLDER = "The path given to the folder is invalid"
     ROOT = "/"
     NEW_FOLDER = {"files": {}}
+    BOOTED_ERROR = "You may want to turn on the OS\n"
+    INVALID_FOLDER = "The path given to the folder is invalid\n"
 
     def __init__(self,
         architecture: str = "x64",
@@ -44,18 +44,18 @@ class OS:
     def status(method):
         def wrapper(self, *args, **kwargs):
             if not self.booted:
-                return "Device has to be on"
+                return self.BOOTED_ERROR
             return method(self, *args, **kwargs)
         return wrapper
 
     def boot(self) -> str:
         self.booted = not self.booted
-        status = "Turning on" if self.booted else "Turning off"
-        return status
+        return "Turning on" if self.booted else "Turning off"
 
     @status
     def enable_graph_enviroment(self):
         self.graph = not self.graph
+        return "Enabling graphical enviroment" if self.booted else "Disabling graphical enviroment"
 
     @status
     def change_name(self, name: str) -> str:
@@ -68,28 +68,26 @@ class OS:
         return f"Your ip address is now {self.public_ip}"
 
     @status
-    def install_packages(self, *packages: str) -> list:
-        error_msgs = []
-        for package in packages:
-            if package not in self.packages:
-                self.packages.append(package)
-            else:
-                error_msg = f"Unable to install {package}, package already installed"
-                if error_msg not in error_msgs:
-                    error_msgs.append(error_msg)
-        return error_msgs
-
-    @status
-    def uninstall_packages(self, *packages:str) -> list:
+    def install_packages(self, *packages: str) -> str:
         error_msgs = []
         for package in packages:
             if package in self.packages:
-                self.packages.remove(package)
-            else:
+                error_msg = f"Unable to install {package}, package already installed"
+                if error_msg not in error_msgs:
+                    error_msgs.append(error_msg)
+            self.packages.append(package)
+        return "\n".join(error_msgs)
+
+    @status
+    def uninstall_packages(self, *packages:str) -> str:
+        error_msgs = []
+        for package in packages:
+            if package not in self.packages:
                 error_msg = f"Unable to uninstall {package}, package not found"
                 if error_msg not in error_msgs:
                     error_msgs.append(error_msg)
-        return error_msgs
+            self.packages.remove(package)
+        return "\n".join(error_msgs)
     
     @status
     def generate_path(self, path: str) -> tuple:
@@ -130,14 +128,14 @@ class OS:
                     if file not in path1:
                         return False
                     _folder = path1[file]
-                    path2[file] = _folder
                     del path1[file]
+                    path2[file] = _folder
                 else:
                     if file not in path1["files"]:
                         return False
                     archive = path1["files"][path1["files"].index(file)]
-                    path2["files"].append(archive)
                     path1["files"].remove(file)
+                    path2["files"].append(archive)
             case _:
                 return False
         return True
@@ -149,29 +147,28 @@ class OS:
         path1, status1 = self.generate_path(folder_path)
         if not status1:
             error_msgs.append(self.INVALID_FOLDER)
-            return error_msgs
+            return "".join(error_msgs)
         match operation_type:
             case "mover":
                 path2, status2 = self.generate_path(new_folder_path)
                 if not status2:
-                    return
+                    error_msgs.append(self.INVALID_FOLDER)
+                    return "".join(error_msgs)
                 for file in files:
                     error = False
-                    if path1 != path2:
-                        status = self.manage_files(operation_type, path1, file, folder, path2=path2)
-                        error = True if not status else False
+                    status = self.manage_files(operation_type, path1, file, folder, path2=path2)
                     if error:
-                        error_msgs.append(f"Something went wrong with the {file_type} {file}")
+                        error_msgs.append(f"Something went wrong with the {file_type} {file}\n")
             case _:
                 for file in files:
                     status = self.manage_files(operation_type, path1, file, folder)
                     if not status:
-                        error_msgs.append(f"Something went wrong with the {file_type} {file}")
-        return error_msgs
+                        error_msgs.append(f"Something went wrong with the {file_type} {file}\n")
+        return "".join(error_msgs)
 
 xubuntu = OS()
 
-print(xubuntu.boot())
+xubuntu.boot()
 print(xubuntu.private_ip)
 print(xubuntu.operate_files("crear", "hola_mundo.py", folder_path="/home/user"))
 print(xubuntu.operate_files("crear", "hola_mundo", folder_path="/etc", folder=True))
