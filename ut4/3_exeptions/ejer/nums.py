eedaf6602bc531a9b4071e97aad56af7c4b5792c
ElPayo <elpayov2@gmail.com>
2023-05-12 16:25:20 +0100

    cambios

--- ut4/3_exeptions/ejer/nums.py
+++ ut4/3_exeptions/ejer/nums.py
@@ -1,8 +1,22 @@
-while isinstance(num := input("Give me an integer number: "), str):
+# while isinstance(num := input("Give me an integer number: "), str):
+#    try:
+#        num = int(num)
+#    except ValueError:
+#        print("Try again")
+#    else:
+#        print(num)
+#        break
+
+
+def get_num():
     try:
-        num = int(num)
+        num = int(input("Give a number: "))
     except ValueError:
         print("Try again")
+        return get_num()
     else:
-        print(num)
-        break
+        return num
+
+
+a = get_num()
+print(a)
